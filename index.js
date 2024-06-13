b84e4c586a59abc096830220b2e4f7a7f0c79406
jjohngrey <john1grey9@gmail.com>
2024-06-13 18:22:39 +0800

    Temporary Add in File to remove node_modules

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ app.get("/", (req, res) => {
     res.render("index.ejs"); 
 });
 
+app.get("/about", (req, res) => { 
+    res.render("about.ejs"); 
+});
+
+app.get("/teams", (req, res) => { 
+    res.render("teams.ejs"); 
+});
+
+app.get("/sponsors", (req, res) => { 
+    res.render("sponsors.ejs"); 
+});
+
+app.get("/recruitment", (req, res) => { 
+    res.render("recruitment.ejs"); 
+});
+
 app.listen(port, () => { 
     console.log(`Server running on port ${port}`); 
 });
